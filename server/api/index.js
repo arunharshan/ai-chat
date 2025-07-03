302b8ac6edc4d7ca859e421636eeb08605603c4d
arun harshan <arunharshan@users.noreply.github.com>
2025-07-03 13:11:33 +0530

    may be a fix

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,4 @@
-const app = require("../index"); // Import the app with .handler exported
 const serverless = require("serverless-http");
+const app = require("../index"); // Import the app with .handler exported
 
 module.exports = serverless(app);
